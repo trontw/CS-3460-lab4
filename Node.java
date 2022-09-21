d0d2a68733b75ae7741f26fd08e9ae5f033914a8
root <root@student2.cs.appstate.edu>
2022-09-21 13:56:18 -0400

    Finished hash1

---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node {
+	public int object;
+	public Node next;
+
+	public Node (int o, Node n) {
+		object = o;
+		next = n;
+	}
+}  
---
+++ Prog.java
@@ -0,0 +1,65 @@
+import java.lang.Integer;
+
+public class Prog {
+	public static HashFunctions hash = null;
+
+	public static void main(String[] args) {
+
+		if (args.length != 2) {
+			System.out.println("Please execute: java Prog <n> <p>");
+			System.out.println("n is the input size, and p is the program number.");
+			System.exit(0);
+		}
+
+		int n = Integer.parseInt(args[0]);
+		int p = Integer.parseInt(args[1]);
+		hash = new HashFunctions(n);
+		switch (p) {
+			case 1:
+				prog1(n);
+				break;
+			case 2:
+				prog2(n);
+				break;
+			case 3:
+				prog3(n);
+				break;
+			case 4:
+				prog4(n);
+				break;
+			default:
+				System.out.println("Invalid program number. Only 1-4.");
+		}
+	}
+
+	private static void prog1(int n) {
+		// TODO: Code to generate n keys that all get hashed to
+		// the same index using hash1.
+		int max = n * n;
+		// System.out.println("Max is " + max);
+		// Loop until reach max
+		for (int i = 0; i < max; ++i) {
+			// Call hash1 and return only mods of input n
+			int modulo = hash.hash1(i);
+			if (modulo == 0) {
+				// Print out only those keys which have the same output (zero)
+				System.out.println(i);
+			}
+		}
+	}
+
+	private static void prog2(int n) {
+		// TODO: Code to generate n keys that all get hashed to
+		// the same index using hash2.
+	}
+
+	private static void prog3(int n) {
+		// TODO: Code to generate n keys that all get hashed to
+		// the same index using hash3.
+	}
+
+	private static void prog4(int n) {
+		// TODO: Code to generate n keys that all get hashed to
+		// the same index using hash4.
+	}
+}
